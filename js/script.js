074995ecb3791886c595f8706059b6eab5cfa524
Flor Werbrouck <flor.werbrouck@student.howest.be>
2022-06-13 22:26:58 +0200

    Update about section

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var sin = Math.sin;
 var floor = Math.floor;
 var abs = Math.abs;
 var pow = Math.pow;
+const doc = document.documentElement;
 
 const changeFavicon = (rgb) => {
     rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
@@ -10,11 +11,12 @@ const changeFavicon = (rgb) => {
         return ("0" + parseInt(x).toString(16)).slice(-2);
     }
     var color = hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+    doc.style.setProperty('--changing-color-dark', '#' + LightenDarkenColor(color, -45));
     document.childNodes[1].childNodes[0].childNodes[1].href = "data:image/svg+xml,%3Csvg id='Favicon' data-name='Capa 1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 58 62.52'%3E%3Cdefs%3E%3Cstyle%3E.cls-1%7Bfill:%23" + color +";%7D.cls-2%7Bfont-size:56.19px;font-family:RooneySans-Bold, RooneySans;font-weight:700;%7D%3C/style%3E%3C/defs%3E%3Cpath class='cls-1' d='M48,58H10A10,10,0,0,1,0,48V10A10,10,0,0,1,10,0H48A10,10,0,0,1,58,10V48A10,10,0,0,1,48,58Z'/%3E%3Ctext class='cls-2' transform='translate(14.9 48.47)'%3EF%3C/text%3E%3C/svg%3E";
 }
 
 // Today in Bad Variable Names, we visit something I chose to call “epoch”…	
-var epoch = function() { return new Date().getTime()/2000; }
+var epoch = function() { return new Date().getTime()/800; }
 // The last number in that line controls color cycle speed.
 
 // This is an adaptation of the rainbow function described at
@@ -37,17 +39,47 @@ function recolor(plus) {
     var cycle = ((epoch()/50) + plus) % 1;
     var color = K(cycle);
     color = 'rgb(' + color.join(',') + ')';
-    $('.o-intro').css({'background-color': color});
+    doc.style.setProperty('--changing-color', color);
     changeFavicon(color);
 }
 
 function appHeight() {
-    const doc = document.documentElement
     doc.style.setProperty('--vh', (window.innerHeight*.01) + 'px');
 }
+
+function LightenDarkenColor(col, amt) {
   
+    var usePound = false;
   
+    if (col[0] == "#") {
+        col = col.slice(1);
+        usePound = true;
+    }
+ 
+    var num = parseInt(col,16);
+ 
+    var r = (num >> 16) + amt;
+ 
+    if (r > 255) r = 255;
+    else if  (r < 0) r = 0;
+ 
+    var b = ((num >> 8) & 0x00FF) + amt;
+ 
+    if (b > 255) b = 255;
+    else if  (b < 0) b = 0;
+ 
+    var g = (num & 0x0000FF) + amt;
+ 
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+ 
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+  
+}
+
+
 $(document).ready(function(){
+    AOS.init();
     window.addEventListener('resize', appHeight);
     appHeight();
     recolor(0);
